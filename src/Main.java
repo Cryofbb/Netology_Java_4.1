e525c07affbc25a2004a815b2b540772d2c11778
Cryofbb <78804095+Cryofbb@users.noreply.github.com>
2021-05-28 13:28:41 +0300

    Added class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int money = 11004;
-        int miles = money / 20;
-        System.out.println("Накоплено " + miles + " миль");
+        BonusMilesService service = new BonusMilesService();
+        int price = 11_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
