45c161f739cc9bb6b426e37ec7fbdae5f749a932
Cryofbb <78804095+Cryofbb@users.noreply.github.com>
2021-05-28 13:14:12 +0300

    Initial

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int money = 11004;
+        int miles = money / 20;
+        System.out.println("Накоплено " + miles + " миль");
+    }
+}
